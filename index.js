97606e8e53ecb88b95bbdd50541e31b9b7a2c6a4
Gecko403 <quangnguyen7403@gmail.com>
2023-04-05 21:55:51 +0700

    2nd commit

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const humbergerMenu = document.getElementsByClassName("humbergerMenu")[0];
 humbergerMenu.addEventListener("click", () =>{
   mobileNav.classList.toggle("active");
 })
+const userBtn = document.getElementsByClassName("userIcon");
+for(let i = 0; i < userBtn.length; i++){
+  userBtn[i].addEventListener('click',()=>{
+  const content = document.getElementsByClassName("userInformations")[i];
+  if(content.style.display == 'none'){
+    content.style.display = 'flex';
+  }else{
+    content.style.display = "none"
+  }
+  
+  })
+}
