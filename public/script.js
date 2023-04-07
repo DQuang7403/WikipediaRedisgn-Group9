3d05a8a4ad0a6851a9c844c0e45b780e8c5f8e71
Gecko403 <quangnguyen7403@gmail.com>
2023-04-07 13:26:01 +0700

    test

---
+++ public/script.js
@@ -0,0 +1,81 @@
+let ourRequest = new XMLHttpRequest();
+ourRequest.open("GET", `https://wikipediaredesign9-default-rtdb.firebaseio.com/user/oVWiQoy4TBQllKIKECitmMaVXvC3.json`);
+ourRequest.send();
+ourRequest.onload = function(){
+let ourData = JSON.parse(ourRequest.responseText)
+console.log(ourData.userName)
+for(let i = 0; i < 2; i++){
+  document.getElementsByClassName("name")[i].innerHTML = ourData.userName;
+  document.getElementsByClassName("email")[i].innerHTML = ourData.email;  
+}
+
+}// On  this day animation
+const buttons = document.getElementsByClassName("visibleEvent");
+const popUpInfo = document.getElementsByClassName("eventInfo");
+const arrows = document.getElementsByClassName("arrow");
+for(let i = 0; i < buttons.length; i++){
+  let popUp = popUpInfo[i];
+  let button = buttons[i];
+  let arrow = arrows[i];
+  button.addEventListener('click',() => {
+    button.classList.toggle('open');
+    if(popUp.style.maxHeight){
+      popUp.style.maxHeight = null;
+      arrow.style.transform = "rotate(0deg)"
+    }else{
+      popUp.style.maxHeight = popUp.scrollHeight + 'px';
+      arrow.style.transform = "rotate(180deg)";
+    }
+  })
+}
+// today article sliding animation
+const AshiftBtns = document.querySelectorAll(".shiftBtn");
+const AWidth = document.querySelector(".Article");
+const Aslider = document.getElementsByClassName("slider")[0];
+const defaultWidth = AWidth.clientWidth;
+for(let i = 0; i < AshiftBtns.length; i++){
+  const shiftBtn = AshiftBtns[i];
+  shiftBtn.addEventListener('click', () =>{
+    if(shiftBtn.id == "right"){
+      Aslider.scrollLeft += defaultWidth ;
+    }else{
+      Aslider.scrollLeft -= defaultWidth;
+    }
+  })
+}
+// did you know animation 
+const DYKshiftBtns = document.querySelectorAll(".DYKshiftBtn");
+const DYKwidth = document.querySelector(".DYKarticle");
+const slider = document.getElementsByClassName("DYKcontainer")[0];
+const DYKarticleWidth = DYKwidth.clientWidth;
+for(let i = 0; i < DYKshiftBtns.length; i++){
+  const DYKBtn = DYKshiftBtns[i];
+  DYKBtn.addEventListener('click', ()=>{
+    if(DYKBtn.id == 'DYKright'){
+      slider.scrollLeft += DYKarticleWidth;
+    }else{
+      slider.scrollLeft -= DYKarticleWidth;
+    }
+  })
+}
+
+const mobileNav = document.getElementsByClassName("mobileNav")[0];
+const humbergerMenu = document.getElementsByClassName("humbergerMenu")[0];
+humbergerMenu.addEventListener("click", () =>{
+  mobileNav.classList.toggle("active");
+})
+
+const userBtn = document.getElementsByClassName("userIcon");
+for(let i = 0; i < userBtn.length; i++){
+  userBtn[i].addEventListener('click',()=>{
+  const content = document.getElementsByClassName("userInformations")[i];
+  if(content.style.display == 'none'){
+    content.style.display = 'flex';
+  }else{
+    content.style.display = "none"
+  }
+  
+  })
+}
+
+
